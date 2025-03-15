d559a972d5ae8d9d7d4e7fde8d3e56ab9831df45
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-03-15 16:26:57 -0700

    develop: controller auto validation for one input

--- src/app/payment/personal.tsx
+++ src/app/payment/personal.tsx
@@ -3,21 +3,29 @@ import { View, StyleSheet } from 'react-native'
 import { router } from "expo-router";
 import CustomButton from '../../components/custom-button';
 import CustomInput from '../../components/custom-input';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
 
 const Personal = () => {
+    const {handleSubmit, formState: {errors}, control} = useForm();
+    console.log(errors)
     const onNext = () => {
         router.push("/payment/payout");
-    }
+    } 
   return (
     <View style={styles.container}>
-      <CustomInput label={'Fullname'} />
+      <Controller 
+        control={control} 
+        name="fullname" 
+        render={({field: {onChange,onBlur,value}})=>(<CustomInput label={'Fullname'} onChangeText={onChange} onBlur={onBlur} value={value}/>)} 
+        rules={{required:"Fullname is required"}}
+      />
       <CustomInput label={'Address'} />
       <View style={{flexDirection : "row", gap: 5}}>
-        <CustomInput label={'City'} style={{flex:1}}/>
+        <CustomInput label={'City'} style={{flex:1}}/>r
         <CustomInput label={'Postal Code'} style={{flex:1}}/>
       </View>
       <CustomInput label={'Phone number'} inputMode="tel"/>
-      <CustomButton title={'Go to payout'} onPress={onNext} style={styles.button}/>
+      <CustomButton title={'Go to payout'} onPress={handleSubmit(onNext)} style={styles.button}/>
     </View>
   )
 }
