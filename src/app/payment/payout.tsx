9c4490600cdfd1d67e99e8eeea6ba670975591f6
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-03-16 00:08:25 -0700

    develop: a fully payout form with zod validation

--- src/app/payment/payout.tsx
+++ src/app/payment/payout.tsx
@@ -2,15 +2,31 @@ import React from 'react'
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { router } from "expo-router";
 import CustomButton from '../../components/custom-button';
+import CustomInput from '../../components/custom-input';
+import { useForm, SubmitHandler, Controller, FormProvider } from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PayoutSchema } from "../../types/payout-schema";
+import * as z from "zod";
+
+type Payout = z.infer<typeof PayoutSchema>;
 
 const PayoutScreen = () => {
-    const onNext = () => {
-        router.push("/payment/summary");
-        };
+  const form = useForm<Payout>({
+    resolver: zodResolver(PayoutSchema),
+  })
+  const onNext: SubmitHandler<Payout> = () => {
+      router.push("/payment/summary");
+  };
   return (
     <View style={styles.container}>
-      <Text>PayoutScreen</Text>
-      <CustomButton title={'Go to summary'} onPress={onNext} style={styles.button}/>
+      <FormProvider {...form}>
+        <CustomInput label={'Card number'} name="cardnumber" inputMode="numeric"/>
+        <View style={{flexDirection : "row" , gap : 5}}>
+          <CustomInput label={'Expiry date'} name="expiry" style={{flex:1}} inputMode="numeric"/>
+          <CustomInput label={'CVV'} name="cvv" inputMode="numeric"/>
+        </View>
+        <CustomButton title={'Go to summary'} onPress={form.handleSubmit(onNext)} style={styles.button}/>
+      </FormProvider>
     </View>
   )
 }
